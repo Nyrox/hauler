54996e198a22e4079f343c04454ed63a0ac96864
Mark Junge - Nyrox <mark.oliver@hotmail.de>
2019-02-03 16:47:45 +0100

    Refactors the crawler into it's own module. Makes the crawler more robust by using the .NET Uri API instead of parsing manually. The Crawler now parses the HTML Document properly instead of doing a scuffed Regex search.

---
+++ src/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Net.Http
+
+open System.Text.RegularExpressions
+
+
+[<EntryPoint>]
+let main argv =
+
+    crawler.run ["http://news.ycombinator.com"; "http://4chan.org"]
+
+    0 // return an integer exit code
---
+++ src/crawler.fs
@@ -0,0 +1,100 @@
+module crawler
+    open System
+    open System.Net.Http
+    open System.Web
+    open System.Net
+
+    open HtmlAgilityPack
+
+    let OUTPUT_BASE_PATH = "index/"
+    let ERROR_LOG_PATH = "error.log"
+
+    let error_log (error: Exception) =
+        System.IO.File.WriteAllText(ERROR_LOG_PATH, error.Message)
+
+    /// Convert's a URI String to a slug by replacing all valid URI characters that are "dangerous" to safe alternatives
+    /// Note that this only takes care of the host and path section of a URI, not the protocol section
+    let slug (uri: string): string =
+        uri |> String.map (fun c -> match c with
+            | '/' -> '_'
+            | _ -> c
+        )
+
+
+    let uri_index_directory_path (uri: Uri): string =
+        assert uri.IsAbsoluteUri
+
+        OUTPUT_BASE_PATH + uri.Host + "/" + (slug uri.AbsolutePath)
+
+    let uri_site_directory_path (uri: Uri): string =
+        assert uri.IsAbsoluteUri
+
+        OUTPUT_BASE_PATH + "sites/" + uri.Host
+
+    let parse (html: string): string seq =
+        let mutable hrefs = List.empty
+
+        let document = new HtmlDocument ()
+        document.LoadHtml html
+
+        let hyperlinks = document.DocumentNode.SelectNodes("//a")
+
+        if hyperlinks = null then Seq.empty 
+        else
+            for node in hyperlinks do
+                let href = node.Attributes.Item "href"
+                hrefs <- match href.Value with
+                    | "" -> hrefs
+                    | _ -> (
+                        "\t-" + href.Value |> Console.WriteLine
+                        href.Value::hrefs
+                    )
+        
+            Seq.cast hrefs
+
+    let get_absolute_uri (base_uri: Uri) (link: string): Uri =
+        if link.StartsWith("//") then (new Uri(link))
+        else if link.StartsWith("/") then (new Uri(base_uri, link))
+        else if link.Contains("://") then (new Uri(link))
+        else (new Uri(base_uri, link))
+
+    /// <summary>
+    /// Given a URI crawls that URI and adds it to the index
+    /// </summary>
+    /// <param name="recrawl">If set to false, will check if the page has been crawled before and if so skip it</param>
+    let crawl (uri: Uri) (recrawl: bool): Uri seq =
+        assert uri.IsAbsoluteUri
+        if not recrawl && System.IO.Directory.Exists (uri_site_directory_path uri) then Seq.empty
+        else
+            Console.WriteLine ("Crawling: " + uri.AbsoluteUri)
+
+            let httpClient = new HttpClient ()
+
+            try 
+                let task = httpClient.GetAsync uri.AbsoluteUri
+                let response = task.Result.Content
+
+                let raw_links = parse (response.ReadAsStringAsync().Result)
+
+                System.IO.Directory.CreateDirectory (uri_site_directory_path uri) |> ignore
+
+                raw_links |> Seq.map (get_absolute_uri uri)
+            with
+                 | ex -> 
+                    ex |> error_log
+                    Seq.cast List.empty
+    
+
+    let run (seeds: string seq) =
+        let mutable open_list = List.empty
+        for s in seeds do open_list <- (new Uri(s))::open_list
+
+        while not open_list.IsEmpty do
+            let new_seeds = crawl open_list.Head false
+            open_list <- open_list.Tail
+            
+            for s in new_seeds do
+                open_list <- s::open_list
+
+
+
